96efb15fe19a427cd699ab2390c03c9d25273d8f
easyelavazhagi-byte <easyelavazhagi@gmail.com>
2026-04-01 10:20:35 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+OUTPUT :
+
+
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+================ RESTART: C:\Users\ELAVAZHAGI\Documents\3.py ================
+Enter a type of shape: (square, rectangle, triangle, pyramid, circle): circle
+Size: 5
+Your shape:
+
+
+        * * * * *
+     * * * * * * * *
+   * * * * * * * * * *
+ * * * * * * * * * * * *
+   * * * * * * * * * *
+     * * * * * * * *
+        * * * * *
